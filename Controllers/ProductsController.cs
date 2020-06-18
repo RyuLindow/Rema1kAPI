ae94d9e64f8e81d04814db7eaeb4f000e968211c
Darek <das@umbraco.dk>
2020-06-18 12:53:38 +0200

    Whole crud

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -24,14 +24,14 @@ namespace Rema1k.Controllers
             _context.Database.EnsureCreated();
         }
         [HttpGet]
-        //Retrieve all products and returns them together with an HTTP status code in the browser/Postman etc. as an array
+        //Retrieve all products and returns them together with an HTTP status code in Postman etc. as an array
         public async Task<IActionResult> GetProducts()
         {
             return Ok(await _context.Products.ToArrayAsync());
         }
 
         [HttpGet("{id}")]
-        //Retrieve a specific product and return it together with an HTTP status code in the browser/Postman etc. as an array
+        //Retrieve a specific product and return it together with an HTTP status code in Postman etc.
         public async Task<IActionResult> GetProduct(int id)
         {
             var product = await _context.Products.FindAsync(id);
@@ -42,13 +42,64 @@ namespace Rema1k.Controllers
             return Ok(product);
         }
 
-
-        /*
         [HttpPost]
+        //Posts a product and returns and returns an HTTP status code in Postman etc.
+        public async Task<ActionResult<Product>> PostProduct([FromBody]Product product)
+        {
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(
+                "GetProduct",
+                new { id = product.Id },
+                product
+            );
+        }
+
+
 
         [HttpPut("{id}")]
+        public async Task<IActionResult> PutProduct([FromRoute] int id, [FromBody] Product product)
+        {
+            if (id != product.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(product).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (_context.Products.Find(id) == null)
+                {
+                    return NotFound();
+                }
+
+                throw;
+            }
+
+            return NoContent();
+        }
 
         [HttpDelete("{id}")]
-        */
+        //Deletes a specific product and returns an HTTP status code in Postman etc.
+        //If an Id for a product that was delete just a moment ago is used, you'll see the Not FOund error - 404
+        public async Task<ActionResult<Product>> DeleteProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+
+            return product;
+        }
     }
 }
